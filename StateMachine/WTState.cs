259093308435dc68982f792c282d34c5d665b6b2
Drew Gingerich <gingerich93@gmail.com>
2020-01-27 07:17:56 -0800

    WIP: state machine

---
+++ StateMachine/WTState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class WTState<T> : ScriptableObject
+{
+    public Action<T> Enter = null;
+    public Func<T, IEnumerator> EnterRoutine = null;
+    public Action<T> Update = null;
+    public Action<T> Exit = null;
+    public Func<T, WTState<T>> GetNextState = null;
+
+    public WTState(
+        Action<T> Enter,
+        Func<T, IEnumerator> EnterRoutine,
+        Action<T> Update,
+        Action<T> Exit,
+        Func<T, WTState<T>> GetNextState
+    )
+    {
+        this.Enter = Enter;
+        this.EnterRoutine = EnterRoutine;
+        this.Update = Update;
+        this.Exit = Exit;
+        this.GetNextState = GetNextState;
+    }
+}
---
+++ StateMachine/WTStateMachine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class WTStateMachine<T> : MonoBehaviour
+{
+    public bool useUnityUpdate = false;
+    public T data;
+
+    private bool waiting = false;
+    private WTState<T> state;
+    private Coroutine enterRoutine;
+
+
+    public void Update()
+    {
+        if (useUnityUpdate)
+            StateUpdate();
+    }
+
+    public void StateUpdate()
+    {
+        if (waiting)
+            return;
+
+        var nextState = this.state.GetNextState(data);
+        if (nextState != null)
+            ChangeState(nextState);
+    }
+
+    public void ChangeState(WTState<T> nextState, bool waitForRoutine=false)
+    {
+        if (waitForRoutine)
+        {
+            StartCoroutine(ChangeOnRoutineEnd(nextState));
+            return;
+        }
+
+        if (enterRoutine != null)
+            StopCoroutine(enterRoutine);
+
+        while(nextState != null)
+        {
+            state.Exit(data);
+            nextState.Enter(data);
+            if (nextState.EnterRoutine != null)
+                StartCoroutine(RunEnterRoutine(nextState.EnterRoutine));
+            state = nextState;
+            nextState = state.GetNextState(data);
+        }
+    }
+
+    private IEnumerator ChangeOnRoutineEnd(WTState<T> nextState)
+    {
+        waiting = true;
+        while (enterRoutine != null)
+            yield return null;
+        ChangeState(nextState);
+        waiting = false;
+    }
+
+    private IEnumerator RunEnterRoutine(Func<T, IEnumerator> routine)
+    {
+        enterRoutine = StartCoroutine(routine(data));
+        yield return enterRoutine;
+        enterRoutine = null;
+    }
+}
